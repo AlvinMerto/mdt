b01a50d64caf96ddc20ad54c458b5c9b3808992b
Alvin Merto <ajbmerto@gmail.com>
2025-08-20 15:51:28 +0800

    08202025

--- resources/views/front/mpap/mpapfront.blade.php
+++ resources/views/front/mpap/mpapfront.blade.php
@@ -9,7 +9,6 @@
 <script>
     var url   = "{{url('tracker')}}";
     var asset = "{{asset('tracker')}}";
-    alert(url);
 </script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
